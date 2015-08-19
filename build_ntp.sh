92507ebde5df24b44d935ba5b15401ebf2eeb53a
Tomasz Flendrich <t.flendrich@gmail.com>
2015-08-19 06:01:05 +0200

    now it will finally install autotools

--- build_ntp.sh
+++ build_ntp.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -x
 touch COPYRIGHT sntp/scm-rev
-pgk install autotools
 env ASSUME_ALWAYS_YES=YES pkg bootstrap
+env ASSUME_ALWAYS_YES=YES pkg install autotools 
 ./bootstrap
 ./build --enable-local-libevent
 cat $(find . -name "make.log")
