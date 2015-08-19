8b063e1dc2904f7bc8af9af99df5de2cd749643b
Tomasz Flendrich <t.flendrich@gmail.com>
2015-08-19 06:11:17 +0200

    fixed a bug in freebsd build script

--- build_ntp.sh
+++ build_ntp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x
 touch COPYRIGHT sntp/scm-rev
-env ASSUME_ALWAYS_YES=YES pkg bootstrap
+env ASSUME_ALWAYS_YES=YES /usr/sbin/pkg bootstrap
 env ASSUME_ALWAYS_YES=YES pkg install autotools 
 ./bootstrap
 ./build --enable-local-libevent
