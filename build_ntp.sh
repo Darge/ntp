39af0b820de67e251f0771d87751a7ed575b9a71
Tomasz Flendrich <t.flendrich@gmail.com>
2015-08-19 05:27:06 +0200

    now it will actually build on freebsd

---
+++ build_ntp.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -x
+touch COPYRIGHT sntp/scm-rev
+./bootstrap
+./build --enable-local-libevent
+cat $(find . -name "make.log")
+cd $(find . -name "A.*")
+make
+make check
+make distcheck
+logout
